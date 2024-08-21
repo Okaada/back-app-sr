0c39950344175a4c863e9a16602f38f3d54f6c4d
Jonatas Rocha <jowrocha07@hotmail.com>
2024-08-21 19:54:26 -0300

    Justed last commit

--- back-app-sr-Application/Tab/Query/GetAllTabs/GetAllTabsQueryHandler.cs
+++ back-app-sr-Application/Tab/Query/GetAllTabs/GetAllTabsQueryHandler.cs
@@ -1,21 +1,21 @@
-// using back_app_sr_Application.Tab.Service.Interface;
-// using back_app_sr_Application.Tab.ViewModel;
-// using MediatR;
-//
-// namespace back_app_sr_Application.Tab.Query.GetAllTabs;
-//
-// public class GetAllTabsQueryHandler : IRequestHandler<GetAllTabsQuery, IEnumerable<TabViewModel>>
-// {
-//     private readonly ITabService _tabService;
-//
-//     public GetAllTabsQueryHandler(ITabService tabService)
-//     {
-//         _tabService = tabService;
-//     }
-//     
-//     public async Task<IEnumerable<TabViewModel>> Handle(GetAllTabsQuery request, CancellationToken cancellationToken)
-//     {
-//         var result = await _tabService.GetAllTabs();
-//         return result;
-//     }
-// }
+using back_app_sr_Application.Tab.Service.Interface;
+using back_app_sr_Application.Tab.ViewModel;
+using MediatR;
+
+namespace back_app_sr_Application.Tab.Query.GetAllTabs;
+
+public class GetAllTabsQueryHandler : IRequestHandler<GetAllTabsQuery, IEnumerable<TabViewModel>>
+{
+    private readonly ITabService _tabService;
+
+    public GetAllTabsQueryHandler(ITabService tabService)
+    {
+        _tabService = tabService;
+    }
+    
+    public async Task<IEnumerable<TabViewModel>> Handle(GetAllTabsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _tabService.GetAllTabs();
+        return result;
+    }
+}

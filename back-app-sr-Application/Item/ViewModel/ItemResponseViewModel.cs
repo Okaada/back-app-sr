3532d013925c59d0699e64f72677d30bbe1ffaad
Ravel Okada <ravelokada@gmail.com>
2024-08-26 23:58:08 -0300

    minor adjusts

--- back-app-sr-Application/Item/ViewModel/ItemResponseViewModel.cs
+++ back-app-sr-Application/Item/ViewModel/ItemResponseViewModel.cs
@@ -4,14 +4,9 @@ namespace back_app_sr_Application.Item.ViewModel;
 
 public class ItemResponseViewModel
 {
-    [JsonProperty("item_id")]
-    public int ItemId { get; set; }
-    [JsonProperty("name")]
-    public string Name { get; set; } = string.Empty;
-    [JsonProperty("value")]
-    public decimal Value { get; set; }
-    [JsonProperty("description")] 
-    public string Description { get; set; }
-    [JsonProperty("is_active")]
-    public bool IsActive { get; set; }
+    [JsonProperty("item_id")] public int ItemId { get; set; }
+    [JsonProperty("name")] public string Name { get; set; } = string.Empty;
+    [JsonProperty("value")] public decimal Value { get; set; }
+    [JsonProperty("description")] public string Description { get; set; } = string.Empty;
+    [JsonProperty("is_active")] public bool IsActive { get; set; }
 }
